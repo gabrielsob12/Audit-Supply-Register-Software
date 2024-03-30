4950f117f9bb00009de6c34dea4f3a57b498b463
gabrielsob12 <gabrielsob2121@gmail.com>
2024-03-30 14:30:03 -0400

    d

--- Copy_paste-script.py
+++ Copy_paste-script.py
@@ -5,10 +5,13 @@ import pandas as pd
 #1 etapa: coletar dados do csv e cadastrar auditorias    
 try:
     #leitura do banco de dados csv e armazenamento
-    audit_table = pd.read_csv("Auditorias.csv", dtype={"Rej. Int. (%)": str, "Lotes Reprovados (%)": str})
-    
+    audit_table = pd.read_csv("Auditorias.csv").astype(str)
     #pode utilizar o print para mostrar tabela
     #print(products_table)
+
+    #click no app da barra de tarefas
+    time.sleep(0.5)
+    pyautogui.click(x=1375, y=1055)
 
     #para coletar os dados do csv, utilizamos o loop em FOR
     for line in audit_table.index:
@@ -17,7 +20,8 @@ try:
         time.sleep(0.5)
         pyautogui.click(x=1204, y=482)
         pyautogui.write(audit_table.loc[line, "Dia"])
-
+        print("oi chegou aqui??")
+        
         #campo data
         time.sleep(0.5)
         pyautogui.write(audit_table.loc[line, "Mês"])
@@ -40,10 +44,8 @@ try:
 
         #registrar auditoria
         time.sleep(0.5)
-        pyautogui.click(x=1042, y=769)
-        pyautogui.press('enter')
-
-    
+        pyautogui.click(x=1037, y=770)
+        pyautogui.click(x=104, y=197)
 except Exception as e:
     print(f"Ocorreu um erro: {e}")
     print("O programa foi encerrado")
